20fc8f55e70e592a9c43ea17bd8f4c528c0f5648
Jackson-Pike <jacksonmpike@gmail.com>
2026-05-12 09:12:34 -1000

    Begin to add state to function, remove props from Board

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,5 +1,23 @@
-function Square({ value }) { // This will pass in a value from board
-  
+/* 
+As a next step, you want the Square component to “remember” that it got clicked, 
+and fill it with an “X” mark. 
+To “remember” things, components use state.
+
+React provides a special function called useState that you can 
+call from your component to let it “remember” things. Let’s
+store the current value of the Square in state, and change 
+it when the Square is clicked.
+
+Import useState at the top of the file. Remove the 
+value prop from the Square component. Instead, add 
+a new line at the start of the Square that calls useState.
+ Have it return a state variable called value:
+
+*/
+import { useState } from 'react';
+
+function Square() { // This will pass in a value from board
+  const [value, setValue] = useState(null);
 
   function handleClick() {
     console.log('clicked!');
@@ -24,19 +42,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square   />
+        <Square   />
+        <Square   />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square   />
+        <Square   />
+        <Square   />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square   />
+        <Square   />
+        <Square   />
       </div>
     </>
     );
