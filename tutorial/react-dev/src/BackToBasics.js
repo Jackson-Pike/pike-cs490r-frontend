0ee8dbba878b2c759ba8df786b3348a7dede2754
Jackson-Pike <jacksonmpike@gmail.com>
2026-05-27 19:37:37 -1000

    Start Learning about useState

--- tutorial/react-dev/src/BackToBasics.js
+++ tutorial/react-dev/src/BackToBasics.js
@@ -5,7 +5,6 @@ function MyButton() { // -- This is a react component - piece of UI, has its own
 }
 
 
-
 export default function MyApp({user}) { 
     // Because this is default export, it can be imported in other places, under any name like import MyComponent from './BackToBasics' and MyComponent will map to MyApp
     // By doing {user}, you get the user object itself. if you did (user) like i had at first, user in that context actually becomes the entire props object. 
