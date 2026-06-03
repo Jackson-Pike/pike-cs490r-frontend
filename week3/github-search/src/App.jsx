167e8e23b84bb1258f4c7a97086b252c0c847387
Jackson-Pike <jacksonmpike@gmail.com>
2026-06-02 22:07:27 -1000

    feat: load movies from backend into console

--- week3/github-search/src/App.jsx
+++ week3/github-search/src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 export default function App() {
+  useEffect(() => {
+    async function loadMovies() {
+      const url = 'http://localhost:3000/api/movies'
+      try {
+        const response = await fetch(url, {
+          headers: {
+              "x-api-key": import.meta.env.VITE_PIKEAPI_KEY
+          }
+        });
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+        
+      }
+      loadMovies()
+
+  }, [])
   return (
     <div className="app">
-     
+      
     </div>
   )
 }
