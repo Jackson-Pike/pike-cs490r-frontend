dc500ad8ea99935e8c4a9060ac8a0f04048a2056
Jackson-Pike <jacksonmpike@gmail.com>
2026-06-02 22:20:40 -1000

    feat: continue hooking to api, save movies in state

--- week3/github-search/src/App.jsx
+++ week3/github-search/src/App.jsx
@@ -3,32 +3,43 @@ import { useState, useEffect } from 'react'
 
 
 export default function App() {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
   useEffect(() => {
     async function loadMovies() {
       const url = 'http://localhost:3000/api/movies'
       try {
+        setLoading(true)
         const response = await fetch(url, {
           headers: {
               "x-api-key": import.meta.env.VITE_PIKEAPI_KEY
           }
         });
+
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
 
         const result = await response.json();
-        console.log(result);
-      } catch (error) {
-        console.error(error.message);
-      }
+        setMovies(result)
+        console.log(result)
         
+        
+      } catch (error) {
+        setError(error.message)
+        console.error(error.message);
+      } finally {
+        setLoading(false);
       }
-      loadMovies()
+    }
+    loadMovies()
 
   }, [])
   return (
     <div className="app">
-      
+        
     </div>
   )
 }
