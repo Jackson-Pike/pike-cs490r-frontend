5875cfbe2b37ac556ecf0109bcb7ae9b29c3f927
Jackson-Pike <jacksonmpike@gmail.com>
2026-06-02 20:54:11 -1000

    Feat: Form: Username field and submit button

--- week3/github-search/src/App.jsx
+++ week3/github-search/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { useState } from 'react'
+
 
 export default function App() {
+  const [searchKey, setSearchKey] = useState('')
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input name="username" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+        <button type="submit" onSubmit={(e) => { e.preventDefault(); console.log(searchKey)}}>Search</button>
+      </form>
     </div>
   )
 }
