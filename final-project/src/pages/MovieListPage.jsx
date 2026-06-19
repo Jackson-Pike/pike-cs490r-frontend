3b7aa560f65f7c8a8bcd2a941e702c69d4025f14
Jackson-Pike <jacksonmpike@gmail.com>
2026-06-18 16:06:19 -1000

    Create useFetch hook to reuse easier. Still some errors

--- final-project/src/pages/MovieListPage.jsx
+++ final-project/src/pages/MovieListPage.jsx
@@ -1,38 +1,19 @@
 import { useState, useEffect } from 'react'
 import MovieCard from '../components/MovieCard'
 import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 
 export default function MovieListPage() {
 
-  const [movies, setMovies] = useState([])
+  //const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
   // DONE(student): implemented useEffect fetch — loads movies from API with loading/error/finally state pattern
-  useEffect(() => {
-    async function loadMovies() {
-        const url = 'http://localhost:3000/api/movies'
+  const url = 'http://localhost:3000/api/movies'
 
-        try {
-            setLoading(true)
-            const response = await fetch(url)
+  const { data: movies, loading, error } = useFetch(url)
 
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-
-            const result = await response.json();
-            setMovies(result)
-            console.log(result)
-        } catch (error) {
-            setError(error.message)
-            console.error(error.message)
-        } finally {
-            setLoading(false);
-        }
-    }
-    loadMovies()
-  }, [])
   return (
     <main className="content">
       {loading ? (
