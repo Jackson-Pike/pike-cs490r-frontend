9d1f7d458ac6be53525ca2bbbbf4b4aa58b2ff94
Jackson-Pike <jacksonmpike@gmail.com>
2026-06-18 16:35:12 -1000

    functionality - more work to movie card

--- final-project/src/pages/MovieDetailPage.jsx
+++ final-project/src/pages/MovieDetailPage.jsx
@@ -1,6 +1,15 @@
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
+import MovieCard from '../components/MovieCard'
 
 export default function MovieDetailPage() {
     const {id} = useParams()
 
+    const url = `http://localhost:3000/api/movies/${id}`
+    const {movie} = useFetch(url)
+
+    if (!movie) return <p>Loading...</p>
+    return <MovieCard movie={data} />
+
+    
 }
